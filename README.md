5997b120dc7ce547fb9fc1c0db01bd412f6ef8af
Sam Samai <rumi@studio2pi.com.au>
2015-08-12 21:45:52 +1000

    Format the Monitor table

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Dine
 
 ** Start the Dining Philosophers process **
 iex --sname party -S mix
+iex> Dine.run
 
 ** Start the monitor process **
 iex --sname monitor -S mix
+iex> Monitor.stats
--- lib/monitor.ex
+++ lib/monitor.ex
@@ -7,15 +7,62 @@ defmodule Monitor do
   def stats() do
     n = Enum.map( @names, fn( name ) -> 
             eats = GenServer.call( { String.to_atom( name ), :"party@sams-mini" }, :how_many_eats, 10000 )
-            { name, eats }
+            [name: name, eats: Integer.to_string(eats)]
           end 
         )
 
     IO.puts IO.ANSI.clear
     IO.puts IO.ANSI.home
-    n |> Enum.map( fn{ name, eats } -> IO.puts "#{name} : #{eats}" end )
+    IO.puts "\n\n\n\n\n\n\n\n\n\n\n"
+    print_table_for_columns( n, [:name, :eats] )
 
-    :timer.sleep( 1500 )
+    :timer.sleep( 500 )
     stats 
   end
+
+  import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1 ]
+
+  def print_table_for_columns(rows, headers) do 
+    data_by_columns = split_into_columns(rows, headers) 
+    column_widths = widths_of(data_by_columns) 
+    format = format_for( column_widths )
+    puts_one_line_in_columns headers, format
+    IO.write "                            "
+    IO.puts separator(column_widths)
+    puts_in_columns data_by_columns, format
+  end
+
+  def split_into_columns(rows, headers) do 
+    for header <- headers do
+      for row <- rows, do: printable(row[header]) 
+    end
+  end
+
+  def printable(str) when is_binary(str), do: str
+  def printable(str), do: to_string(str)
+
+  def widths_of(columns) do
+    for column <- columns, do: column |> map(&String.length/1) |> map( fn(x) -> if x < 5, do: 5, else: x end ) |> max
+  end
+
+  def format_for(column_widths) do
+    map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
+  end
+
+  def separator(column_widths) do
+    map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
+  end
+
+  def puts_in_columns(data_by_columns, format) do 
+    data_by_columns
+    |> List.zip
+    |> map(&Tuple.to_list/1)
+    |> each(&puts_one_line_in_columns(&1, format))
+  end
+
+  def puts_one_line_in_columns(fields, format) do
+    IO.write "                            "
+    :io.format(format, fields)
+  end
+
 end
